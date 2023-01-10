cb231273c8184b819902ca59c1e7e04b509a03f1
Seas <122377269+S3455@users.noreply.github.com>
2023-01-11 00:05:58 +0100

    Add files via upload

---
+++ Boucles.r
@@ -0,0 +1,18 @@
+Boucles
+
+Ex 3
+
+Var nbr en entier
+Début       
+     Nbr <- 0
+    Ecrire « Entrez un nombre de départ »
+    Lire nombre
+    Pour i allant de num+1 à num+10
+        Ecrire i 
+    Fin pour
+Fin
+
+Ex 4
+
+
+
